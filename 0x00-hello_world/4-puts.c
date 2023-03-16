0a69baa893274188644d78165d1f02377ca94564
Adenike Agunbiade <sholayewole@gmail.com>
2023-03-16 08:18:46 -0700

    my first c codes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-/*
- * main - printing a line of code using puts
- * return: 0
- */
+/**
+ * main - prints a line of code using puts
+ *
+ * Return: 0
+*/
+
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-/*
+/**
  * main - printing a line of code with printf
- * return: 0
+ *
+ * Return: 0
  */
 int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
