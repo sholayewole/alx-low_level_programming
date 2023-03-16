847c6e7570b31c9b80ddc3b99bb0761f18902368
Adenike Agunbiade <sholayewole@gmail.com>
2023-03-16 13:41:05 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,11 @@
 /**
  * main - program unix
  *
- * Return: 0
+ * Return: 1
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
-	return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+return (1);
 }
