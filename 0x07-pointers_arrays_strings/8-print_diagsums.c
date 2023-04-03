77d853446aae5fbfc916ad0359269067ed1370e0
Adenike Agunbiade <sholayewole@gmail.com>
2023-04-03 13:57:20 -0700

    task 7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,15 +9,22 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, sum1 = 0, sum2 = 0;
+	int sum1 = 0, sum2 = 0;
+	int i, j;
 
-	for (i = 0; i < size * size; i += size + 1)
+	for (1 = 0; i < size; i++)
 	{
-		sum1 += a[i];
-	}
-	for (i = size - 1; i < size * size - 1; i += size - 1)
-	{
-		sum2 += a[i];
+		for (j = 0; j < size; j++)
+		{
+			if (i == j)
+			{
+				sum1 += *(a + i * size + j);
+			}
+			if (i == size - j - 1)
+			{
+				sum2 += *(a + i * size + j);
+			}
+		}
 	}
 	printf("Sum of diagonal 1: %d\n", sum1);
 	printf("Sum of diagonal 2: %d\n", sum2);
