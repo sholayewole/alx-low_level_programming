840eee985a4aec94bb5175414bb16792a10d3fdb
Adenike Agunbiade <sholayewole@gmail.com>
2023-04-05 13:52:43 -0700

    task 0

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,6 +8,3 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
-int _sqrt_helper(int n, int i);
-int is_prime_helper(int n, int i)
-int is_palindrome_helper(char *s, int i, int j)
