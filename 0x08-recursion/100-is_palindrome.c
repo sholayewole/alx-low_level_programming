46388895ec0c1b8a1248c53f527dbb70c40e507e
Adenike Agunbiade <sholayewole@gmail.com>
2023-04-05 15:30:48 -0700

    task 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -3,6 +3,22 @@
 int is_palindrome_helper(char *s, int i,  int j);
 
 /**
+ * is_palindrome - function that returns 1
+ * if a string is a palindrome and 0 if not
+ * @s: string
+ *
+ * Return: 1 or 0
+ */
+
+int is_palindrome(char *s)
+{
+	int len = strlen(s);
+	{
+		return (is_palindrome_helper(s, 0, len - 1));
+	}
+}
+
+/**
  * is_palindrome_helper  - helper function for is_palindrome
  * @s: number
  * @i: int
@@ -21,20 +37,4 @@ int is_palindrome_helper(char *s, int i, int j)
 		return (0);
 	}
 	return (is_palindrome_helper(s, i + 1, j - 1));
-}
-
-/**
- * is_palindrome - function that returns 1
- * if a string is a palindrome and 0 if not
- * @s: string
- *
- * Return: 1 or 0
- */
-
-int is_palindrome(char *s)
-{
-	int len = strlen(s);
-	{
-		return (is_palindrome_helper(s, 0, len - 1));
-	}
 }
