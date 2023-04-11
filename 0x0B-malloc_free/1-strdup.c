9b7ae57f2d8be71ac5aa4f461063e1a701dfca33
Adenike Agunbiade <sholayewole@gmail.com>
2023-04-11 09:12:03 -0700

    task 0-6

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -21,11 +21,11 @@ char *_strdup(char *str)
 	while (str[len] != '\0')
 		len++;
 
-	dup_str  = maloc((len + 1) * sizeof(char));
+	dup_str  = malloc((len + 1) * sizeof(char));
 	if (dup_str == NULL)
 		return (NULL);
 
 	for (i = 0; i <= len; i++)
-		dup_str[i} = str[i];
+		dup_str[i] = str[i];
 	return (dup_str);
 }
